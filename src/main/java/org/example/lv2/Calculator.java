f90b818b8bf845a7d8a0ab79db33429b1aee4375
김영훈 <91935220+younghunkimm@users.noreply.github.com>
2025-07-08 14:51:53 +0900

    refactor: lv1의 계산기능 Calculator 클래스에 분리

---
+++ src/main/java/org/example/lv2/Calculator.java
@@ -0,0 +1,53 @@
+package org.example.lv2;
+
+import java.util.ArrayList;
+
+public class Calculator {
+    // === 속성 ===
+    protected int firstNumber; // 첫 번째 숫자
+    protected int secondNumber; // 두 번째 숫자
+    protected char operator; // 연산자
+
+    public boolean isOperate = true; // 계산 가능 여부
+
+    // 연산 결과를 저장하는 컬렉션 타입 필드 선언 및 생성
+    private ArrayList<Integer> list = new ArrayList<>();
+
+    // === 생성자 ===
+    public Calculator(int firstNumber, int secondNumber, char operator) {
+        this.firstNumber = firstNumber;
+        this.secondNumber = secondNumber;
+        this.operator = operator;
+    }
+
+    // === 기능 ===
+    public int calculate() {
+        int result = 0;
+
+        switch (operator) {
+            case '+':
+                result = firstNumber + secondNumber;
+                break;
+            case '-':
+                result = firstNumber - secondNumber;
+                break;
+            case '*':
+                result = firstNumber * secondNumber;
+                break;
+            case '/':
+                if (secondNumber == 0) {
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                    isOperate = false;
+                } else {
+                    result = firstNumber / secondNumber;
+                }
+                break;
+            default:
+                System.out.println("올바른 연산자를 입력해주세요.");
+                isOperate = false;
+                break;
+        }
+
+        return result;
+    }
+}
--- src/main/java/org/example/lv2/Main.java
+++ src/main/java/org/example/lv2/Main.java
@@ -1,4 +1,34 @@
 package org.example.lv2;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int firstNumber = sc.nextInt();
+
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int secondNumber = sc.nextInt();
+
+            System.out.print("사칙연산 기호를 입력하세요(+,-,*,/): ");
+            char operator = sc.next().charAt(0); // String 을 받은 후 char 형으로 변환
+
+            Calculator calculator = new Calculator(firstNumber, secondNumber, operator);
+            int result = calculator.calculate();
+
+            if (calculator.isOperate) {
+                System.out.println("결과 = " + result);
+            } else {
+                continue;
+            }
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            if (sc.next().equals("exit")) break;
+        }
+
+        sc.close();
+    }
 }
