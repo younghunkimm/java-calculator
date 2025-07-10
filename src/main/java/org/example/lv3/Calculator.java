4dfb6e0eb70b508a2e75901a011ad72710b2b7a2
김영훈 <91935220+younghunkimm@users.noreply.github.com>
2025-07-10 15:22:46 +0900

    feat: lv3 - 저장된 연산 결과 중 사용자 입력 값보다 큰 값들을 조회하는 기능

--- src/main/java/org/example/lv3/Calculator.java
+++ src/main/java/org/example/lv3/Calculator.java
@@ -2,6 +2,8 @@ package org.example.lv3;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
 public class Calculator {
     // 속성
     private final List<Double> resultList = new ArrayList<>(); // 결과값이 담길 컬렉션
@@ -22,5 +24,12 @@ public class Calculator {
     // 결과값을 컬렉션에 넣는 기능
     public void addResult(double num) {
         resultList.add(num);
+    }
+
+    // 저장된 연산 결과들 중 사용자가 입력한 값보다 큰 값들을 출력하는 기능
+    public List<Double> printExceedingValues(double inputValue) {
+        return resultList.stream()
+                .filter(result -> result > inputValue)
+                .collect(Collectors.toList());
     }
 }
